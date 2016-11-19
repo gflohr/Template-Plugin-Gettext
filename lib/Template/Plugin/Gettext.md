9461c4b190e6d6dbeb0ba96f68368869a069e548
Guido Flohr <guido.flohr@cantanea.com>
2016-11-19 11:47:38 +0200

    docs, wip

--- lib/Template/Plugin/Gettext.md
+++ lib/Template/Plugin/Gettext.md
@@ -4,8 +4,34 @@ Template::Plugin::Gettext - Gettext Support For the Template Toolkit Version 2
 
 # SYNOPSIS
 
-In your templates:
+Load the plug-in in templates:
 
-    [% USE gettext(com.textdomain.my utf-8 DIRECTORIES...) %]
+    [% USE Gettext('com.textdomain.my' 'utf-8' 'DIRECTORIES'...) %]
+    [% Gettext.gettext('Hello, world!) %]
+
+Or alias "Gettext":
+
+    [% USE gtx = Gettext('com.textdomain.my' 'utf-8' 'DIRECTORIES'...) %]
+    [% gtx.gettext('Hello, world!) %]
+
+Use method invocations:
+
+    [% Gettext.gettext("Hello, world!") %]
+    [% Gettext.xgettext("Hello, {name}!", name => 'John Doe') %]
+
+Or filters:
+
+    [% FILTER gettext %]
+    Hello, world!
+    [% END %]
+
+    [% FILTER xgettext(name => 'John Doe') %]
+    Hello, {name}!
+    [% END %]
 
 # DESCRIPTION
+
+The **Template::Plugin::Gettext** plug-in makes the
+[GNU gettext API](https://www.gnu.org/software/gettext/) available for
+documents using the
+[Template Toolkit version 2](http://template-toolkit.org/).
--- lib/Template/Plugin/Gettext.pm
+++ lib/Template/Plugin/Gettext.pm
@@ -442,10 +442,35 @@ Template::Plugin::Gettext - Gettext Support For the Template Toolkit Version 2
 
 =head1 SYNOPSIS
 
-In your templates:
+Load the plug-in in templates:
 
-    [% USE gettext(com.textdomain.my utf-8 DIRECTORIES...) %]
+    [% USE Gettext('com.textdomain.my' 'utf-8' 'DIRECTORIES'...) %]
+    [% Gettext.gettext('Hello, world!) %]
+
+Or alias "Gettext":
+
+    [% USE gtx = Gettext('com.textdomain.my' 'utf-8' 'DIRECTORIES'...) %]
+    [% gtx.gettext('Hello, world!) %]
+
+Use method invocations:
+
+    [% Gettext.gettext("Hello, world!") %]
+    [% Gettext.xgettext("Hello, {name}!", name => 'John Doe') %]
+
+Or filters:
+
+    [% FILTER gettext %]
+    Hello, world!
+    [% END %]
+
+    [% FILTER xgettext(name => 'John Doe') %]
+    Hello, {name}!
+    [% END %]
 
 =head1 DESCRIPTION
 
+The B<Template::Plugin::Gettext> plug-in makes the
+L<GNU gettext API|https://www.gnu.org/software/gettext/> available for
+documents using the
+L<Template Toolkit version 2|http://template-toolkit.org/>.
 
