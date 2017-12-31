709ff2dcbc4d6c3dfd144509efe3b19001c278c7
Guido Flohr <guido.flohr@cantanea.com>
2017-12-31 11:50:28 +0200

    allow setting input filename for textdomains

--- lib/Template/Plugin/Gettext.pm
+++ lib/Template/Plugin/Gettext.pm
@@ -61,6 +61,12 @@ sub new {
     $charset = 'utf-8' unless defined $charset && length $charset;
 
     my $template = $ctx->stash->get('component')->name;
+    if ('input text' eq $template || 'input file handle' eq $template) {
+            $DB::single = 1;
+        my $maybe_template = $ctx->stash->get('gettext_filename');
+        $template = $maybe_template
+            if defined $maybe_template && length $maybe_template;
+    }
     $textdomains{$textdomain}->{$template} = 1;
 
     unless (exists $bound_dirs{$textdomain}) {
